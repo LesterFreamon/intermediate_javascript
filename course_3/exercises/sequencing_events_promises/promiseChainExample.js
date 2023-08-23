7c2f11d0e7442fb13a63e4e083c03e2574f34631
Adam Arnon <adamarnon@gmail.com>
2023-08-23 11:18:28 -0700

    promises

---
+++ course_3/exercises/sequencing_events_promises/promiseChainExample.js
@@ -0,0 +1,10 @@
+const genericAPIrequest = () => {
+    return new Promise((resolve, reject) => {
+         resolve({body: "My test data"})
+     })
+ }
+
+ 
+ genericAPIrequest().then(data => {
+     console.log(data);
+ }).catch(error => console.log(error));
